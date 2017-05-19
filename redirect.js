38bbacd7dd5d6e4bef1eddfc02c40b02255e5016
Ailyn <lynzkieann016@gmail.com>
2017-05-19 14:29:00 +0800

    :cocktail: Javascript Redirect
    Using window.location to Redirect to a Different URL with JavaScript

---
+++ redirect.js
@@ -0,0 +1,17 @@
+// Sets the new location of the current window.
+window.location = "http://www.example.com";
+ 
+// Sets the new href (URL) for the current window.
+window.location.href = "http://www.example.com";
+ 
+// Assigns a new URL to the current window.
+window.location.assign("http://www.example.com");
+ 
+// Replaces the location of the current window with the new one.
+window.location.replace("http://www.example.com");
+ 
+// Sets the location of the current window itself.
+self.location = "http://www.example.com";
+ 
+// Sets the location of the topmost window of the current window.
+top.location = "http://www.example.com";
