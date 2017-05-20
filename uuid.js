b803e6991b4f33fd843d29992586b1b8ee59909b
Ailyn <lynzkieann016@gmail.com>
2017-05-20 11:59:05 +0800

    :video_game: Random UUID
    A UUID(Universally Unique Identifier), it can be used for any purpose, from user authentication string to payment transaction id.
    
    A UUID is a 16-octet (128-bit) number. In its canonical form, a UUID is represented by 32 hexadecimal digits, displayed in five groups separated by hyphens, in the form 8-4-4-4-12 for a total of 36 characters (32 alphanumeric characters and four hyphens).

---
+++ uuid.js
@@ -0,0 +1,13 @@
+function generate_uuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0x0C2f ) | 0x4000,
+        mt_rand( 0, 0x3fff ) | 0x8000,
+        mt_rand( 0, 0x2Aff ), mt_rand( 0, 0xffD3 ), mt_rand( 0, 0xff4B )
+    );
+
+}
+
+//calling function
+$transationID = generate_uuid();
